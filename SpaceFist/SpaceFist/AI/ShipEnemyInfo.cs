668892ca16f23fe36816a1d7c7d4517ff0b15a06
Andrew Smith <smith.andrew.l@gmail.com>
2014-04-02 16:25:59 -0500

    Added properties to ShipEnemyInfo and ShipInfo

--- SpaceFist/SpaceFist/AI/ShipEnemyInfo.cs
+++ SpaceFist/SpaceFist/AI/ShipEnemyInfo.cs
@@ -2,24 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace SpaceFist.AI
 {
     class ShipEnemyInfo : FuzzyLogicEnabled
     {
+        // Distance
+        private int distance;
+
+        // Rotation Difference
+        private int rotationTo;  
+
         // ship movement in relation to the enemy
-        // away, towards, not moving
-        // visible
-        // line of sight to ship
-        // intercept velocity
-        // time to intercept
-        // point of interception
-        // has fired towards us
-        // ship facing us
+        public double ShipMovingAway    { get; set; }
+        public double ShipMovingTowards { get; set; }
+        public double ShipNotMoving     { get; set; }
 
         // Distance
-        // very close, near, intermediate, far, very far
-        // 
+        public double ShipVeryClose { get; set; }
+        public double ShipNear      { get; set; }
+        public double ShipAway      { get; set; }
+        public double ShipFar       { get; set; }
+        public double ShipVeryFar   { get; set; }
+
+        // visible
+        public bool ShipVisible { get; set; }
+
+        public double ShipFacingAway      { get; set; }
+        public double ShipFacingElseWhere { get; set; }
+        public double ShipFacingUs        { get; set; }
+
+        // Interception information
+        public int     TimeToIntercept   { get; set; }
+        public Vector2 LineOfSight       { get; set; }
+        public Vector2 InterceptVelocity { get; set; }
+        public Vector2 InterceptPoint    { get; set; }
+
+
 
         public override void Update()
         {
--- SpaceFist/SpaceFist/AI/ShipInfo.cs
+++ SpaceFist/SpaceFist/AI/ShipInfo.cs
@@ -3,44 +3,84 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace SpaceFist.AI
 {
     public class ShipInfo : FuzzyLogicEnabled
     {
-        private Game game;
-        private EnemyManager enemyManager;
-        private BlockManager blockManager;
+        private Game              game;
+        private EnemyManager      enemyManager;
+        private BlockManager      blockManager;
         private ProjectileManager projectileManager;
 
         // raw data
-        // heading
-        // health
-        // player acc
-        // shots fired per minute
-        // weapon damage
-        // power-ups used
-        // score
-        // kills per minute
-        // collisions per minute
-        // current speed
-        // possible target
+        private Vector2 heading;
+        private float   health;
+        private float   acc;
+        private float   shotsPerMin;
+        private float   weaponDamage;
+        private int     powerUpsUsed;
+        private int     score;
+        private int     killsAMinute;
+        private int     collisionsPerMinute;
+        private int     speed;
+
+        private Entity possibleTarget;
 
         // fuzzy values
-        // Very low health, low health, medium health, high health, full health
-        // Very low acc, low acc, medium acc, high acc, perfect acc
-        // Never shoots, some times shoots, trigger happy, maniac
-        // low power weapon, normal power weapon, high power weapon
-        // Never uses pickups, sometimes uses pickups, uses all pickups
-        // No kills, so so, many, crazy
+        
+        // The players health
+        public double VeryLowHealth { get; set; }
+        public double LowHealth     { get; set; }
+        public double MediumHealth  { get; set; }
+        public double HighHealth    { get; set; }
+        public double FullHealth    { get; set; }
+
+        
+        public double VeryLowAcc { get; set; }
+        public double LowAcc     { get; set; }
+        public double MediumAcc  { get; set; }
+        public double HighAcc    { get; set; }
+        public double PerfectAcc { get; set; }
+
+        // How often the player shoots
+        public double NeverShoots    { get; set; }
+        public double SometimeShoots { get; set; }
+        public double TriggerHappy   { get; set; }
+        public double Maniac         { get; set; }
+
+        // Weapon Strength
+        public double WeakWeapon     { get; set; }
+        public double NormalWeapon   { get; set; }
+        public double PowerfulWeapon { get; set; }
+
+        public double NoKills       { get; set; }
+        public double SomeKills     { get; set; }
+        public double ManyKills     { get; set; }
+        public double VeryManyKills { get; set; }
+
         // perfect, some hits, normal, many hits, bumper cars
+        public double NoBumps     { get; set; }
+        public double SomeBumps   { get; set; }
+        public double NormalBumps { get; set; }
+        public double ManyBumps   { get; set; }
+        public double BumperCars  { get; set; }
+
+
         // stationary, moving slowly, moving quickly, top speed
-        // stationary, seeking health, avoiding block, chasing, evading, unknown
+        public double NotMoving     { get; set; }
+        public double MovingSlowly  { get; set; }
+        public double Moving        { get; set; }
+        public double MovingQuickly { get; set; }
+        public double TopSpeed      { get; set; }
+
         public ShipInfo(Game game, Ship ship, EnemyManager enemyManager, BlockManager blockManager, ProjectileManager projectileManager)
         {
             this.game = game;
-            this.enemyManager = enemyManager;
-            this.blockManager = blockManager;
+
+            this.enemyManager      = enemyManager;
+            this.blockManager      = blockManager;
             this.projectileManager = projectileManager;
         }
 
