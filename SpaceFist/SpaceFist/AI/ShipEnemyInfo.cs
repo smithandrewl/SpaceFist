545f6e554730a7032c6251052031f00a8a5abf48
Andrew Smith <smith.andrew.l@gmail.com>
2014-04-02 14:44:52 -0500

    Added some idea comments to ShipEnemyInfo and ShipInfo

--- SpaceFist/SpaceFist/AI/ShipEnemyInfo.cs
+++ SpaceFist/SpaceFist/AI/ShipEnemyInfo.cs
@@ -7,6 +7,20 @@ namespace SpaceFist.AI
 {
     class ShipEnemyInfo : FuzzyLogicEnabled
     {
+        // ship movement in relation to the enemy
+        // away, towards, not moving
+        // visible
+        // line of sight to ship
+        // intercept velocity
+        // time to intercept
+        // point of interception
+        // has fired towards us
+        // ship facing us
+
+        // Distance
+        // very close, near, intermediate, far, very far
+        // 
+
         public override void Update()
         {
             throw new NotImplementedException();
--- SpaceFist/SpaceFist/AI/ShipInfo.cs
+++ SpaceFist/SpaceFist/AI/ShipInfo.cs
@@ -13,6 +13,29 @@ namespace SpaceFist.AI
         private BlockManager blockManager;
         private ProjectileManager projectileManager;
 
+        // raw data
+        // heading
+        // health
+        // player acc
+        // shots fired per minute
+        // weapon damage
+        // power-ups used
+        // score
+        // kills per minute
+        // collisions per minute
+        // current speed
+        // possible target
+
+        // fuzzy values
+        // Very low health, low health, medium health, high health, full health
+        // Very low acc, low acc, medium acc, high acc, perfect acc
+        // Never shoots, some times shoots, trigger happy, maniac
+        // low power weapon, normal power weapon, high power weapon
+        // Never uses pickups, sometimes uses pickups, uses all pickups
+        // No kills, so so, many, crazy
+        // perfect, some hits, normal, many hits, bumper cars
+        // stationary, moving slowly, moving quickly, top speed
+        // stationary, seeking health, avoiding block, chasing, evading, unknown
         public ShipInfo(Game game, Ship ship, EnemyManager enemyManager, BlockManager blockManager, ProjectileManager projectileManager)
         {
             this.game = game;
