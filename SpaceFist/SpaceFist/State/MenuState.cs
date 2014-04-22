bf7a36114b1f7bbfcf2c71467214dc646a68f95a
Andrew Smith <smith.andrew.l@gmail.com>
2014-04-22 17:08:11 -0500

    Added logo and menu states

---
+++ SpaceFist/SpaceFist/State/MenuState.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using SpaceFist.State.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceFist.State
+{
+    public class MenuState : GameState
+    {
+        private Game      game;
+        private Texture2D background;
+        private Texture2D menu;
+        private Rectangle backgroundRect;
+        private DateTime enteredAt;
+        private Rectangle menuRect;
+        private Rectangle newGameRect;
+        private Rectangle creditsRect;
+        private Rectangle exitRect;
+
+        public MenuState(Game game)
+        {
+            this.game  = game;
+        }
+        public void LoadContent()
+        {
+            var resolution = game.GraphicsDevice.Viewport.Bounds;
+
+            backgroundRect = resolution;
+
+            background = game.BackgroundRed;
+            
+            menu           = game.Menu;
+            menuRect       = new Rectangle((int) ((background.Width / 2f) - (menu.Width)), (int)((background.Height / 2f) - (menu.Height)), menu.Width, menu.Height);
+
+            newGameRect = new Rectangle(menuRect.X + 8, menuRect.Y + 12, 149, 29);
+            creditsRect = new Rectangle(menuRect.X + 8, menuRect.Y + 46, 149, 29);
+            exitRect    = new Rectangle(menuRect.X + 8, menuRect.Y + 82, 149, 29);
+        
+        }
+
+        public void Draw(Microsoft.Xna.Framework.GameTime time)
+        {
+            game.SpriteBatch.Draw(background, backgroundRect, Color.White);
+
+            game.SpriteBatch.Draw(menu, menuRect, Color.White);
+
+        }
+
+        public void Update()
+        {
+            MouseState mouse = Mouse.GetState();
+
+            Point mousePos = new Point(mouse.X, mouse.Y);
+
+            if (mouse.LeftButton == ButtonState.Pressed)
+            {
+                if (newGameRect.Contains(mousePos))
+                {
+                    game.CurrentState = game.InPlayState;
+                }
+
+                if (creditsRect.Contains(mousePos))
+                {
+
+                }
+
+                if (exitRect.Contains(mousePos))
+                {
+                    game.Exit();
+                }
+            }
+        }
+
+        public void EnteringState()
+        {
+            enteredAt = DateTime.Now;
+            game.IsMouseVisible = true;
+
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.Play(game.TitleScreenSong);
+        }
+
+        public void ExitingState()
+        {
+            MediaPlayer.Stop();
+            game.IsMouseVisible = false;
+        }
+    }
+}
