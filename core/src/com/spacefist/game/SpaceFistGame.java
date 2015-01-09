83ea76017ed2048da051aeb9d0d66a3e13b04484
smithandrewl <smith.andrew.l@gmail.com>
2015-01-09 14:24:08 -0600

    Migrated the Menu code should to use the scene.ui framework

--- core/src/com/spacefist/game/SpaceFistGame.java
+++ core/src/com/spacefist/game/SpaceFistGame.java
@@ -52,7 +52,7 @@ public class SpaceFistGame extends ApplicationAdapter {
 
         currentState.update();
 
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         // Tell the current state to draw itself
--- core/src/com/spacefist/state/MenuState.java
+++ core/src/com/spacefist/state/MenuState.java
@@ -2,17 +2,27 @@ package com.spacefist.state;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.spacefist.GameData;
 import com.spacefist.state.abst.GameState;
 
 import java.util.HashMap;
 
- //  TODO: The Menu code should be migrated to use the scene.ui framework
 /**
  * This state displays the main menu and handles input to
  * switch to several other states.
@@ -20,13 +30,14 @@ import java.util.HashMap;
 public class MenuState implements GameState {
     private GameData  gameData;
     private long      enteredAt;
-    private Texture   background;
-    private Texture   menu;
-    private Rectangle backgroundRect;
-    private Rectangle menuRect;
-    private Rectangle newGameRect;
-    private Rectangle creditsRect;
-    private Rectangle exitRect;
+    // private Texture   background;
+    // private Rectangle backgroundRect;
+
+    public static final int BUTTON_PADDING = 3;
+    public static final int BUTTON_WIDTH = 200;
+
+    private Skin  skin;
+    private Stage stage;
 
     public MenuState(GameData gameData) {
         this.gameData = gameData;
@@ -35,78 +46,49 @@ public class MenuState implements GameState {
     @Override
     public void loadContent() {
 
-        backgroundRect = gameData.getResolution();
+        // backgroundRect = gameData.getResolution();
+
         HashMap<String, Texture> textures = gameData.getTextures();
 
-        background     = textures.get("BackgroundRed");
-        menu           = textures.get("Menu");
-
-        menuRect = new Rectangle(
-                (int) ((background.getWidth()  / 2.0f) - (menu.getWidth())),
-                (int) ((background.getHeight() / 2.0f) - (menu.getHeight())),
-                menu.getWidth(), menu.getHeight()
-        );
-
-        // Calculate and set rectangles for each button since the buttons
-        // are part of a single image.
-        newGameRect = new Rectangle(menuRect.x + 8, menuRect.y + 6, 149, 25);
-        creditsRect = new Rectangle(menuRect.x + 8, menuRect.y + 23, 149, 25);
-        exitRect    = new Rectangle(menuRect.x + 8, menuRect.y + 53, 149, 29);
+        // background     = textures.get("BackgroundRed");
     }
 
     @Override
     public void draw() {
+        //TODO: Use scene.ui to render the background image
+
+        /*
         SpriteBatch spriteBatch = gameData.getSpriteBatch();
 
         spriteBatch.draw(
-            background,
-            backgroundRect.x,
-            backgroundRect.y,
-            background.getWidth(),
-            backgroundRect.getHeight()
+                background,
+                backgroundRect.x,
+                backgroundRect.y,
+                background.getWidth(),
+                backgroundRect.getHeight()
         );
+        */
 
-        spriteBatch.draw(
-            menu,
-            menuRect.x,
-            menuRect.y,
-            menuRect.getWidth(),
-            menuRect.getHeight()
-        );
+        stage.draw();
     }
 
     @Override
     public void update() {
 
-        Vector2 mousePos = new Vector2(Gdx.input.getX(), Gdx.input.getY());
-
         boolean safeToProcessInput = (TimeUtils.millis() - enteredAt) > 300;
 
         if (safeToProcessInput) {
-            if (Gdx.input.isTouched()) {
-                if (newGameRect.contains(mousePos)) {
-                    gameData.getInPlayState().loadContent();
-                    gameData.setCurrentState(gameData.getInPlayState());
-                }
+            if (Gdx.input.isKeyPressed(Keys.ENTER)) {
 
-                if (creditsRect.contains(mousePos)) {
-                    gameData.setCurrentState(gameData.getCreditsState());
-                }
+                gameData.getInPlayState().loadContent();
+                gameData.setCurrentState(gameData.getInPlayState());
 
-                if (exitRect.contains(mousePos)) {
-                    Gdx.app.exit();
-                }
-            } else {
-                if (Gdx.input.isKeyPressed(Keys.ENTER)) {
-
-                    gameData.getInPlayState().loadContent();
-                    gameData.setCurrentState(gameData.getInPlayState());
-
-                } else if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
-                    Gdx.app.exit();
-                }
+            } else if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
+                Gdx.app.exit();
             }
         }
+
+        stage.act();
     }
 
     @Override
@@ -118,10 +100,52 @@ public class MenuState implements GameState {
 
         gameData.getLevelManager().Init();
         gameData.getLevelManager().LoadLevel(1);
+
+
+        stage = new Stage();
+        skin = new Skin(Gdx.files.internal("images/ui/uiskin.json"));
+
+        Gdx.input.setInputProcessor(stage);
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        final TextButton newGame = new TextButton("New Game", skin);
+        final TextButton credits = new TextButton("Credits", skin);
+        final TextButton exit    = new TextButton("Exit", skin);
+
+        table.add(newGame).width(BUTTON_WIDTH).pad(BUTTON_PADDING);
+        table.row();
+        table.add(credits).width(BUTTON_WIDTH).pad(BUTTON_PADDING);
+        table.row();
+        table.add(exit).width(BUTTON_WIDTH).pad(BUTTON_PADDING);
+
+        newGame.addListener(new ChangeListener() {
+            public void changed(ChangeEvent event, Actor actor) {
+                gameData.getInPlayState().loadContent();
+                gameData.setCurrentState(gameData.getInPlayState());
+            }
+        });
+
+        credits.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                gameData.setCurrentState(gameData.getCreditsState());
+            }
+        });
+
+        exit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
     }
 
     @Override
     public void exitingState() {
         gameData.getSongs().get("TitleScreen").stop();
+        stage.dispose();
     }
 }
