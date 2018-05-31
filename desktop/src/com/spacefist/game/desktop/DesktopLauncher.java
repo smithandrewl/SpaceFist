8589a62b99b02212374ca1af65cb8da0e42c5667
Andrew Smith <smith.andrew.l@gmail.com>
2018-05-30 20:35:15 -0500

    Migrated from old libGDX to new libGDX projct.

--- desktop/src/com/spacefist/game/desktop/DesktopLauncher.java
+++ desktop/src/com/spacefist/game/desktop/DesktopLauncher.java
@@ -2,17 +2,12 @@ package com.spacefist.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spacefist.game.MyGdxGame;
 import com.spacefist.game.SpaceFistGame;
 
 public class DesktopLauncher {
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-
-        config.width      = 1366;
-        config.height     = 768;
-        config.fullscreen = false;
-        config.title      = "Space Fist";
-
-        new LwjglApplication(new SpaceFistGame(), config);
-    }
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceFistGame(), config);
+	}
 }
