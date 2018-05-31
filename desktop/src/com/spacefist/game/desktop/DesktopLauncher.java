754b19e7d126a6a1a25e3abc2504a26233d5dd94
Andrew Smith <smith.andrew.l@gmail.com>
2018-05-30 20:47:49 -0500

    Fixed resolution.

--- desktop/src/com/spacefist/game/desktop/DesktopLauncher.java
+++ desktop/src/com/spacefist/game/desktop/DesktopLauncher.java
@@ -2,12 +2,17 @@ package com.spacefist.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.spacefist.game.MyGdxGame;
 import com.spacefist.game.SpaceFistGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width      = 1366;
+		config.height     = 768;
+		config.fullscreen = false;
+		config.title      = "Space Fist";
+
 		new LwjglApplication(new SpaceFistGame(), config);
 	}
 }
