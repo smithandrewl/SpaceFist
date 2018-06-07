c89ba7961f5339ed4423b1c4e53d3afe2b7f9d55
Andrew Smith <smith.andrew.l@gmail.com>
2018-06-07 09:14:20 -0500

    Migrated Manager to Kotlin.

---
+++ core/src/com/spacefist/managers/Manager.kt
@@ -0,0 +1,60 @@
+package com.spacefist.managers
+
+import com.badlogic.gdx.utils.Array
+import com.spacefist.GameData
+import com.spacefist.entities.Entity
+
+open class Manager<T : Entity>(protected var gameData: GameData) : Iterable<T> {
+
+    protected var entities: Array<T>
+
+    init {
+        entities = Array(false, 16)
+    }
+
+    protected fun add(entity: T) {
+        assert(entity != null)
+
+        entities.add(entity)
+    }
+
+    protected fun remove(entity: T) {
+        assert(entity != null)
+
+        entities.removeValue(entity, true)
+    }
+
+    open fun update() {
+        for (entity in entities) {
+            if (entity.isAlive) {
+                entity.update()
+            }
+        }
+    }
+
+    fun draw() {
+        for (entity in entities) {
+            entity.draw()
+        }
+    }
+
+    fun collisions(obj: Entity): Iterable<T> {
+        val collisions = Array<T>(false, 16)
+
+        for (entity in entities) {
+            if (entity.isAlive && entity.rectangle.overlaps(obj.rectangle)) {
+                collisions.add(entity)
+            }
+        }
+
+        return collisions
+    }
+
+    fun clear() {
+        entities.clear()
+    }
+
+    override fun iterator(): Iterator<T> {
+        return entities.iterator()
+    }
+}
